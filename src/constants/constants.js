4e03b826ab45c1f0d9f06dc237e1f9fd25f14b19
Телицын Никита <dmtvn.labs@yandex.ru>
2022-01-29 16:59:14 +0300

    Added constants of swapi

---
+++ src/constants/constants.js
@@ -0,0 +1,9 @@
+const HTTP = 'http://';
+const HTTPS = 'https://';
+
+const SWAPI = 'swapi.dev/api';
+const SWAPI_PEOPLE = '/people';
+const SWAPI_FILMS = '/films';
+const SWAPI_PLANETS = '/planets';
+
+export {HTTP, HTTPS, SWAPI_PEOPLE, SWAPI_PLANETS, SWAPI_FILMS, SWAPI};
