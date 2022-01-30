6906553067715350a3db9633bf1102a9cfe9c6fb
Телицын Никита <dmtvn.labs@yandex.ru>
2022-01-30 19:06:37 +0300

    Added Navbar and NavLink

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import './NavBar.scss'
+
+const NavBar = () => {
+    return (
+        <nav className="nav">
+            <NavLink className="nav__item" to='/'>Main</NavLink>
+            <NavLink className="nav__item" to='/characters'>Characters</NavLink>
+            <NavLink className="nav__item" to='/planets'>Planets</NavLink>
+            <NavLink className="nav__item" to='/vehicles'>Vehicles</NavLink>
+        </nav>
+    );
+};
+
+export default NavBar;
