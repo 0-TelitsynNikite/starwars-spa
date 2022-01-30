162c3c1a970d4ae34484ade9975ec7ba677078fa
Телицын Никита <dmtvn.labs@yandex.ru>
2022-01-30 11:25:00 +0300

    Added a functionality of drawing a list of people

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import StarWarsApi from "./services/StarWarsApi";
+import {useEffect, useState} from "react";
+
 function App() {
+  const [people, setPeople] = useState(null);
+
+  const getResource = async () => {
+      const res = await new StarWarsApi().getAllPeople();
+
+      const peopleList = res.map(({name, url}) => {
+          return {
+              name: name,
+              url: url
+          }
+      });
+
+      setPeople(peopleList);
+    }
+
+  useEffect( () => {
+      getResource();
+  }, [])
+
+
   return (
     <div className="main">
-
+        {people && (
+            <ul>
+                {people.map(({name, url}) => <li key={url}>{name}</li>)}
+            </ul>
+        )}
     </div>
   );
 }
