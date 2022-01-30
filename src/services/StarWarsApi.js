2fa3424f20326bc2cda07e55355a3d60672a3f6f
Телицын Никита <dmtvn.labs@yandex.ru>
2022-01-30 11:25:57 +0300

    Changed getAllPeople method

--- src/services/StarWarsApi.js
+++ src/services/StarWarsApi.js
@@ -1,19 +1,14 @@
 import {API_PEOPLE} from "../constants/constants";
 
 export default class StarWarsApi {
-   async getResource(url) {
-       const res = await fetch(url);
-       return await res.json();
-   }
+    async getResource(url) {
+        const res = await fetch(url);
+        return res.json();
+    }
 
-   async getAllPeople() {
-       const res = await this.getResource(API_PEOPLE).then(data => data.results);
-       return res;
-   }
+    async getAllPeople() {
+        const res = this.getResource(API_PEOPLE).then(data => data.results);
+        return res;
+    }
 
-   _transform(prom) {
-        return {
-            name: prom.name
-        }
-   }
 }
