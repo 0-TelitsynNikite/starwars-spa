2f6ef8c4d721af4c451c312b54668500ca7a4932
Телицын Никита <dmtvn.labs@yandex.ru>
2022-01-31 17:45:12 +0300

    Added routing for planets and vehicles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import PeopleList from "./components/PeopleList/PeopleList";
 import MainPage from "./components/MainPage/MainPage";
 import NavBar from "./components/NavBar/NavBar";
+import PlanetsList from "./components/PlanetsList/PlanetsList";
+import VehiclesList from "./components/VehiclesList/VehiclesList";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './styles/index.scss'
+import './styles/index.scss';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
             <Routes>
                 <Route path='/' element={<MainPage/>}/>
                 <Route path='/characters' element={<PeopleList/>}/>
+                <Route path='/planets' element={<PlanetsList/>}/>
+                <Route path='/vehicles' element={<VehiclesList/>}/>
             </Routes>
         </BrowserRouter>
     </div>
